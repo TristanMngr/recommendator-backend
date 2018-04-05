42d18e31d7051822bfffffd0c8425c702beaaf2e
Thibault HENTGES <thibault.hentges@gmail.com>
2018-04-05 11:39:52 +0200

    adding /testEntity route

--- src/main/java/com/mycompany/app/controller/FirstController.java
+++ src/main/java/com/mycompany/app/controller/FirstController.java
@@ -1,14 +1,11 @@
 package com.mycompany.app.controller;
 
+import com.mycompany.app.model.RandomTestEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 
 @RestController
 public class FirstController {
@@ -17,5 +14,13 @@ public class FirstController {
     public ResponseEntity<?> test() {
         HttpHeaders resp_headers = new HttpHeaders();
         return new ResponseEntity<String>("the testing route is working", resp_headers, HttpStatus.OK);
+    }
+
+    // to return json object
+    @RequestMapping("/testEntity")
+    public ResponseEntity<?> testEntity() {
+        HttpHeaders resp_headers = new HttpHeaders();
+        RandomTestEntity test = new RandomTestEntity("testing string", 420);
+        return new ResponseEntity<RandomTestEntity>(test, resp_headers, HttpStatus.OK);
     }
 }
