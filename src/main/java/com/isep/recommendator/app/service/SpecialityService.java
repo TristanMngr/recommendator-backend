29987f1e03215a28b3a8d62cfa7a1e02bc2640a6
TristanMngr <tristanmenager@gmail.com>
2018-05-11 16:37:43 +0200

    Speciality
    routes :
    - [GET] /specialities
    - [POST] /specialities
    - [GET, DELETE] /specialities/{speciality_id}
    - [GET] speciality/{speciality_id}/modules
    - [POST] speciality/{speciality_id}/modules
    - [POST] specialities/{speciality_id}/jobs
    
    + test
    + mini handler

--- src/main/java/com/isep/recommendator/app/service/SpecialityService.java
+++ src/main/java/com/isep/recommendator/app/service/SpecialityService.java
@@ -1,7 +1,105 @@
 package com.isep.recommendator.app.service;
 
+import com.isep.recommendator.app.handler.ResourceNotFoundException;
+import com.isep.recommendator.app.model.Job;
+import com.isep.recommendator.app.model.Module;
+import com.isep.recommendator.app.model.Speciality;
+import com.isep.recommendator.app.model.SpecialityModule;
+import com.isep.recommendator.app.repository.JobRepository;
+import com.isep.recommendator.app.repository.ModuleRepository;
+import com.isep.recommendator.app.repository.SpecialityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SpecialityService {
+    private SpecialityRepository specialityRepository;
+    private ModuleRepository     moduleRepository;
+    private JobRepository        jobRepository;
+
+    @Autowired
+    public SpecialityService(SpecialityRepository specialityRepository, ModuleRepository moduleRepository, JobRepository jobRepository) {
+        this.specialityRepository = specialityRepository;
+        this.moduleRepository = moduleRepository;
+        this.jobRepository = jobRepository;
+    }
+
+    public Speciality create(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    public Speciality getSpeciality(Long specialityId) throws ResourceNotFoundException {
+        Optional<Speciality> speciality = this.specialityRepository.findById(specialityId);
+
+        if (!speciality.isPresent())
+            throw new ResourceNotFoundException("Speciality with id " + specialityId + "does not exist");
+
+        return speciality.get();
+    }
+
+    public Speciality destroy(Long specialityId) throws ResourceNotFoundException {
+        Optional<Speciality> speciality = specialityRepository.findById(specialityId);
+
+        if (!speciality.isPresent()) {
+            throw new ResourceNotFoundException("Speciality with id " + specialityId + " does not exist");
+        }
+
+        for (Job job : speciality.get().getJobs()) {
+            job.getSpecialities().remove(speciality.get());
+        }
+        specialityRepository.delete(speciality.get());
+
+        return speciality.get();
+    }
+
+    public Speciality addModule(Long specialityId, Long moduleId, boolean isMain) throws ResourceNotFoundException {
+        Optional<Module>     module     = moduleRepository.findById(moduleId);
+        Optional<Speciality> speciality = specialityRepository.findById(specialityId);
+
+        if (!module.isPresent()) {
+            throw new ResourceNotFoundException("Module with id " + moduleId + " does not exist");
+        }
+        if (!speciality.isPresent()) {
+            throw new ResourceNotFoundException("Speciality with id " + specialityId + " does not exist");
+        }
+
+        speciality.get().getSpecialityModules().add(new SpecialityModule(speciality.get(), module.get(), isMain));
+        specialityRepository.save(speciality.get());
+
+        return speciality.get();
+    }
+
+    public Speciality addJob(Long specialityId, Long jobId) throws ResourceNotFoundException {
+        Optional<Job>        job        = jobRepository.findById(jobId);
+        Optional<Speciality> speciality = specialityRepository.findById(specialityId);
+
+        if (!job.isPresent()) {
+            throw new ResourceNotFoundException("Job with id " + jobId + " does not exist");
+        }
+        if (!speciality.isPresent()) {
+            throw new ResourceNotFoundException("Speciality with id " + specialityId + " does not exist");
+        }
+
+        speciality.get().getJobs().add(job.get());
+        job.get().getSpecialities().add(speciality.get());
+        specialityRepository.save(speciality.get());
+
+        return speciality.get();
+    }
+
+    public List<Speciality> getAll() {
+        return specialityRepository.findAll();
+    }
+
+    public List<Module> getModulesBySpecialitiesIds(List<Long> specialitiesIds) throws ResourceNotFoundException {
+        List<Module> modules = moduleRepository.findBySpecialitiesIds(specialitiesIds);
+
+        if (modules.isEmpty())
+            throw new ResourceNotFoundException("Specialities with these ids does not exist");
+
+        return modules;
+    }
 }
---
+++ src/test/java/com/isep/recommendator/app/SpecialityControllerTest.java
@@ -0,0 +1,181 @@
+package com.isep.recommendator.app;
+
+import com.isep.recommendator.app.model.Job;
+import com.isep.recommendator.app.model.Module;
+import com.isep.recommendator.app.model.Speciality;
+import com.isep.recommendator.app.repository.JobRepository;
+import com.isep.recommendator.app.repository.ModuleRepository;
+import com.isep.recommendator.app.repository.SpecialityRepository;
+import com.isep.recommendator.security.config.WebSecurityConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class, WebSecurityConfig.class})
+@WebAppConfiguration
+public class SpecialityControllerTest {
+    @Autowired
+    SpecialityRepository specialityRepository;
+    @Autowired
+    ModuleRepository moduleRepository;
+    @Autowired
+    JobRepository jobRepository;
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    private Speciality firstSpeciality;
+    private Speciality secondSpeciality;
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+    private MockMvc mockMvc;
+
+    @Before
+    public void before() {
+        this.mockMvc = webAppContextSetup(webApplicationContext)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+
+        this.specialityRepository.deleteAllInBatch();
+        this.specialityRepository.deleteAllInBatch();
+
+        firstSpeciality = this.specialityRepository.save(new Speciality("speciality1", "descriptionSpeciliaty1"));
+        secondSpeciality = this.specialityRepository.save(new Speciality("speciality2", "descriptionSpeciality2"));
+    }
+
+    @Test
+    // [GET] /specialities
+    public void getAll() throws Exception {
+        mockMvc.perform(get("/specialities")
+                .contentType(contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(firstSpeciality.getId().intValue())))
+                .andExpect(jsonPath("$[0].name", is(firstSpeciality.getName())))
+                .andExpect(jsonPath("$[1].id", is(secondSpeciality.getId().intValue())))
+                .andExpect(jsonPath("$[1].name", is(secondSpeciality.getName())));
+    }
+
+    @Test
+    // [GET] /specialities/{id} - no speciality with this id
+    public void SpecialityNotFound() throws Exception {
+        mockMvc.perform(get("/specilities/100")
+                .contentType(contentType))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    // [GET] /specialities/{id}
+    public void getSpeciality() throws Exception {
+        mockMvc.perform(get("/specialities/" + firstSpeciality.getId())
+                .contentType(contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(firstSpeciality.getId().intValue())))
+                .andExpect(jsonPath("$.name", is(firstSpeciality.getName())))
+                .andExpect(jsonPath("$.description", is(firstSpeciality.getDescription())));
+    }
+
+    @Test
+    // [DELETE] /specialities/{id}
+    @WithMockUser(authorities = {"ADMIN" })
+    public void destroy() throws Exception {
+        Long specialityId = firstSpeciality.getId();
+
+        mockMvc.perform(delete("/specialities/" + specialityId)
+                .contentType(contentType))
+                .andExpect(status().isOk());
+
+        assertFalse(this.specialityRepository.findById(specialityId).isPresent());
+    }
+
+    //TODO to test
+    // [GET] specialities/{specialitiesIds}/modules
+
+    @Test
+    // [POST] specialities/{speciality_id}/modules
+    // add main module
+    @WithMockUser(authorities = {"ADMIN" })
+    public void addMainModuleToSpeciality() throws Exception {
+        Module firstModule = this.moduleRepository.save(new Module("module1", "module1"));
+
+        mockMvc.perform(post("/specialities/" + firstSpeciality.getId() + "/modules")
+                .contentType(contentType)
+                .param("module_id", firstModule.getId().toString())
+                .param("is_main", "true"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("speciality1")));
+
+        //TODO verifier que la liason est faite
+
+    }
+
+    @Test
+    // [POST] specialities/{speciality_id}/modules
+    // ad normal module
+    @WithMockUser(authorities = {"ADMIN" })
+    public void addNormalModuleToSpeciality() throws Exception {
+        Module firstModule = this.moduleRepository.save(new Module("module1", "module1"));
+
+        mockMvc.perform(post("/specialities/" + firstSpeciality.getId() + "/modules")
+                .contentType(contentType)
+                .param("module_id", firstModule.getId().toString())
+                .param("is_main", "false"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("speciality1")));
+
+        //la je test avec le speciality 2 pour le même objet module
+        mockMvc.perform(post("/specialities/" + secondSpeciality.getId() + "/modules")
+                .contentType(contentType)
+                .param("module_id", firstModule.getId().toString())
+                .param("is_main", "false"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("speciality2")));
+
+        //TODO verifier que la liaison est faite
+    }
+
+    @Test
+    // [POST] specialities/{speciality_id}/jobs
+    // ad jobs
+    @WithMockUser(authorities = {"ADMIN" })
+    public void addJobToSpeciality() throws Exception {
+        Job job = this.jobRepository.save(new Job("job1", "job1"));
+
+        mockMvc.perform(post("/specialities/" + firstSpeciality.getId() + "/jobs")
+                .contentType(contentType)
+                .param("job_id", job.getId().toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("speciality1")));
+
+        mockMvc.perform(post("/specialities/" + secondSpeciality.getId() + "/jobs")
+                .contentType(contentType)
+                .param("job_id", job.getId().toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("speciality2")));
+
+
+        //TODO verifier que la liaison est faite
+    }
+}
