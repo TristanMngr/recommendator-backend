183e1dcb6726ca1d177d340fe21734f353992141
TristanMngr <tristanmenager@gmail.com>
2018-05-11 16:47:10 +0200

    Concept
    routes:
    - [GET, POST] /concepts
    - [GET] /concepts/{concept_id}
    - [PUT] /concepts/{concept_id}
    
    +test
    +mini handler

---
+++ src/main/java/com/isep/recommendator/app/controller/ConceptController.java
@@ -0,0 +1,72 @@
+package com.isep.recommendator.app.controller;
+
+import com.isep.recommendator.app.handler.BadRequestException;
+import com.isep.recommendator.app.model.Concept;
+import com.isep.recommendator.app.service.ConceptService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/concepts")
+@Api(value="/concepts", description="All endpoints about concepts")
+public class ConceptController {
+
+    private final ConceptService conceptService;
+
+    @Autowired
+    public ConceptController(ConceptService conceptService){
+        this.conceptService = conceptService;
+    }
+
+
+    @GetMapping("")
+    @ApiOperation(value = "Get all concepts [PUBLIC]", response = Concept.class, responseContainer = "List")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Concept> getAll() {
+        List<Concept> concepts = conceptService.getAll();
+        return concepts;
+    }
+
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Get a concept by id [PUBLIC]", response = Concept.class)
+    public Concept getById(@PathVariable(value = "id") Long id) {
+        Concept concept = conceptService.get(id);
+        return concept;
+    }
+
+
+    @PostMapping("")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ApiOperation(value = "Create a concept [ADMIN]", notes="should be admin" ,response = Concept.class)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Concept create(@RequestParam("name") String name) throws BadRequestException {
+        Concept concept = conceptService.create(name);
+        return concept;
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ApiOperation(value = "Delete a concept [ADMIN]", notes="should be admin")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable(value = "id") Long id){
+        Concept concept = conceptService.get(id);
+        conceptService.delete(concept);
+        return;
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ApiOperation(value = "Update a concept [ADMIN]", notes="should be admin", response = Concept.class)
+    public Concept updateById(@PathVariable(value = "id") Long id, @RequestParam("name") String name) throws BadRequestException {
+        Concept concept = conceptService.get(id);
+        concept = conceptService.update(concept, name);
+        return concept;
+    }
+}
---
+++ src/main/java/com/isep/recommendator/app/service/ConceptService.java
@@ -0,0 +1,64 @@
+package com.isep.recommendator.app.service;
+
+import com.isep.recommendator.app.handler.BadRequestException;
+import com.isep.recommendator.app.handler.CustomValidationException;
+import com.isep.recommendator.app.handler.ResourceNotFoundException;
+import com.isep.recommendator.app.model.Concept;
+import com.isep.recommendator.app.repository.ConceptRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ConceptService {
+
+    private final ConceptRepository conceptRepo;
+
+    @Autowired
+    public ConceptService(ConceptRepository conceptRepo){
+        this.conceptRepo = conceptRepo;
+    }
+
+    public Concept get(Long id){
+        Optional concept = conceptRepo.findById(id);
+        if (concept.isPresent())
+            return (Concept) concept.get();
+        else
+            throw new ResourceNotFoundException("no concept found with id " + id);
+    }
+
+    public List<Concept> getAll(){
+        return conceptRepo.findAll();
+    }
+
+    public Concept create(String name) throws BadRequestException{
+        if (conceptRepo.findByName(name) != null)
+            throw new BadRequestException("concept with name " + name + " already exist");
+
+        try {
+            @Valid Concept concept = new Concept(name);
+            return conceptRepo.save(concept);
+        } catch (ConstraintViolationException e){
+            throw new CustomValidationException(e);
+        }
+
+    }
+
+
+    public void delete(Concept concept){
+        conceptRepo.delete(concept);
+    }
+
+    public Concept update(Concept concept, String new_name) throws BadRequestException{
+        if (conceptRepo.findByName(new_name) != null)
+            throw new BadRequestException("concept with name " + new_name + " already exist");
+
+        concept.setName(new_name);
+        return conceptRepo.save(concept);
+    }
+
+}
