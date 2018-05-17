dce0365893a617d070a74831bf49c117a1fb3664
TristanMngr <tristanmenager@gmail.com>
2018-05-17 15:47:53 +0200

    loadUserByUsername

--- src/main/java/com/isep/recommendator/security/service/CustomUserDetailsService.java
+++ src/main/java/com/isep/recommendator/security/service/CustomUserDetailsService.java
@@ -29,6 +29,8 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userService.findByUsername(username);
 
         if (user == null)
+
+
             throw new UsernameNotFoundException("username not found");
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
@@ -38,4 +40,52 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
     }
+
+    public UserDetails loadUserByUsername(String username, String password) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+        System.out.println("EXISTANT");
+        System.out.println(user.getPassword());
+        System.out.println("Existant end");
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        // ldap
+        if (user == null) {
+            LDAPaccess access = new LDAPaccess();
+            try {
+                User userLdap = access.LDAPget(username, password); // remplacez login par la variable qui contient le login, et mdp par la variable qui contient le mot de passe
+                System.out.println("IN ldap");
+                System.out.println(password);
+                if (userLdap == null)
+                {
+                    System.out.println("login invalide");
+                    System.exit(1);
+                }
+                System.out.println(userLdap.toString());
+                user = userService.createUser(username, password, true);
+
+                grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
+            } catch(Exception e) {
+                System.err.println(e.getMessage());
+                System.exit(1);
+            }
+        }
+        // database
+        else {
+            grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
+            if (user.isAdmin())
+                grantedAuthorities.add(new SimpleGrantedAuthority("ADMIN"));
+        }
+
+    /*if (user == null)
+        throw new UsernameNotFoundException("username not found");
+
+    Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+    grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
+    if (user.isAdmin())
+        grantedAuthorities.add(new SimpleGrantedAuthority("ADMIN"));*/
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+
 }
