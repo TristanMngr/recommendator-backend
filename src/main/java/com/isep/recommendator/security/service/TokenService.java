e9b6a6f74e287d41b49da5de6d5c36c72519c0ae
Thibault HENTGES <thibault.hentges@gmail.com>
2018-05-18 09:38:26 +0200

    removing useless com

--- src/main/java/com/isep/recommendator/security/service/TokenService.java
+++ src/main/java/com/isep/recommendator/security/service/TokenService.java
@@ -69,11 +69,6 @@ public class TokenService {
     }
 
     public static TokenResponseObject getSuccessResponse(String token){
-//        HashMap<String, Object> resp = new HashMap<>();
-//        resp.put("token", token);
-//        resp.put("token_type", TOKEN_PREFIX);
-//        resp.put("token_expiration", EXPIRATION_TIME);
-//        resp.put("full_token", TOKEN_PREFIX + token);
         return new TokenResponseObject(token, EXPIRATION_TIME, TOKEN_PREFIX, TOKEN_PREFIX + token);
     }
 
