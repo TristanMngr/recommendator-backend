5ce7fe1379ef410510d2455388bfeaa09ef3aa48
Thibault HENTGES <thibault.hentges@gmail.com>
2018-06-04 17:47:46 +0200

    authorize CORS on delete requests

--- src/main/java/com/isep/recommendator/security/config/WebSecurityConfig.java
+++ src/main/java/com/isep/recommendator/security/config/WebSecurityConfig.java
@@ -73,7 +73,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
