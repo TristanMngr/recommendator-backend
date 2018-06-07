4f4698122bfa65099341256b550f412e1d2131a1
TristanMngr <tristanmenager@gmail.com>
2018-06-07 17:56:14 +0200

    damn it

--- src/test/java/com/isep/recommendator/app/service/ModuleServiceTest.java
+++ src/test/java/com/isep/recommendator/app/service/ModuleServiceTest.java
@@ -13,12 +13,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class, WebSecurityConfig.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public class ModuleServiceTest {
     @Autowired
     ModuleService moduleService;
